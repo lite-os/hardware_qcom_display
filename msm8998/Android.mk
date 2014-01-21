19615c1577c659e111f1b5a199fe910be32dd6d4
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2014-01-21 23:06:45 +0100

    display: add TARGET_PROVIDES_LIBLIGHT
    Change-Id: I9388f3582d74b4b569e2807e4ec6177dc50f38d0

--- msm8998/Android.mk
+++ msm8998/Android.mk
@@ -2,8 +2,12 @@ sdm-libs := sdm/libs
 display-hals := include libqservice libqdutils $(sdm-libs)/utils $(sdm-libs)/core
 
 ifneq ($(TARGET_IS_HEADLESS), true)
-    display-hals += libcopybit liblight libmemtrack hdmi_cec \
+    display-hals += libcopybit libmemtrack hdmi_cec \
                     $(sdm-libs)/hwc2 gpu_tonemapper libdrmutils
+endif
+
+ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
+    display-hals += liblight
 endif
 
 display-hals += libgralloc1
